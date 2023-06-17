13d97db1e975f4f2e351dbd0d40c9bbcfff72ff2
Joseildo M S filho <joseildo.filho@contaazul.com>
2023-06-17 11:23:15 -0300

    chore: adds an automatic theme change following a window scheduler

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,3 +7,11 @@ require('kanagawa').setup{
 }
 vim.o.background = 'light'
 vim.cmd('colorscheme kanagawa-lotus')
+
+local timer = vim.loop.new_timer()
+timer:start(0, 10000, vim.schedule_wrap(function()
+  local hour = tonumber(os.date('%H'))
+  -- You can change color scheme here.
+  local bg = (hour > 8 and hour < 19) and 'light' or 'dark'
+  if vim.o.bg ~= bg then vim.o.bg = bg end
+end))

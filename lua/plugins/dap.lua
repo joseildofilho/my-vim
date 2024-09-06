5acc00ebd6718687a3c49b6a2f4f21f21dabea52
Joseildo M S filho <joseildo.filho@contaazul.com>
2024-09-05 21:37:29 -0300

    chore: adds hooks for dapui

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,17 +10,17 @@ else
     debugger_path = "/usr/joseildo.filho/.local/share/nvim/lazy/vscode-js-debug"
 end
 
-require'dap-go'.setup{}
+require 'dap-go'.setup {}
 
 require("dap-vscode-js").setup({
     -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-    debugger_path = debugger_path,                                                             -- Path to vscode-js-debug installation.
+    debugger_path = debugger_path,                                                               -- Path to vscode-js-debug installation.
     -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
     adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
     --log_file_path = "(stdpath cache)/dap_vscode_js.log", -- Path for file logging
-    log_file_level = vim.log.levels.ERROR,                                                     -- Logging level for output to file. Set to false to disable file logging.
+    log_file_level = vim.log.levels.ERROR,                                                       -- Logging level for output to file. Set to false to disable file logging.
     log_console_level = vim.log.levels
-    .ERROR                                                                                     -- Logging level for output to console. Set to false to disable console output.
+        .ERROR                                                                                   -- Logging level for output to console. Set to false to disable console output.
 })
 
 --dap.adapters.node = {
@@ -70,3 +70,17 @@ dap.configurations.dart = {
         toolArgs = { "-d", "all" }
     }
 }
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.before.attach.dapui_config = function()
+    dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+    dapui.close()
+end

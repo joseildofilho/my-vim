098bdac6abf44befe6c07e20e79051f02385b9eb
Joseildo Filho <filhojoseildo@gmail.com>
2025-02-24 14:07:10 -0300

    chore: adds new snippets

--- lua/LuaSnip/all.lua
+++ lua/LuaSnip/all.lua
@@ -68,6 +68,28 @@ return {
         )
     ),
     s(
+        { trig = "ere", dscr = "creates the if err != nil block" },
+        fmta(
+            [[
+            if err != nil {
+                return err
+            }
+        ]],
+            {}
+        )
+    ),
+    s(
+        { trig = "ern", dscr = "creates the if err != nil block" },
+        fmta(
+            [[
+            if err != nil {
+                return nil
+            }
+        ]],
+            {}
+        )
+    ),
+    s(
         { trig = "errp", dscr = "creates the if err != nil block that panics" },
         fmta(
             [[
@@ -104,5 +126,49 @@ return {
                 r(1),
             }
         )
-    )
+    ),
+    s(
+        { trig = 'db_value', dscr = 'creates a db value type for golang' },
+        fmta(
+            [[
+                type <> string
+
+                func (v *<>) Scan(value any) error {
+                	field, ok := value.(string)
+                	if !ok {
+                		return errors.New("When scanning <> from db, expected string, got: " + reflect.TypeOf(value).String())
+                	}
+
+                	*v = <>(field)
+                	return nil
+                }
+
+                func (v <>) Value() (driver.Value, error) {
+                	return v.String(), nil
+                }
+
+                func (v <>) String() string {
+                	return string(v)
+                }
+    ]], {
+                i(1),
+                r(1),
+                r(1),
+                r(1),
+                r(1),
+                r(1),
+            }
+        )
+    ),
+    s({ trig = 'func Test_', dscr = 'creates a simple first test for golang' }, fmta(
+        [[
+            func Test_<>(t *testing.T) {
+            	t.Run("should <>", func(t *testing.T) {
+            	})
+            }
+    ]], { i(1), i(2) }
+    )),
+    s({ trig = 't.Run', dscr = 'creates a simple test case for golang' }, fmta(
+        [[ t.Run("should <>", func(t *testing.T) {}) ]], { i(1) }
+    ))
 }

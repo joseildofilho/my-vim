c999e9dac09193bdb73d91c7cd6406945eaf204f
Joseildo Filho <filhojoseildo@gmail.com>
2025-05-30 11:30:52 -0300

    chore: changes the version for luasnip and removes nvim-scissors

--- lua/plugins/snippets/init.lua
+++ lua/plugins/snippets/init.lua
@@ -1,18 +1,11 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    version = "v1.*",
+    version = "v2.*",
     build = "make install_jsregexp",
     config = function()
       require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/lua/LuaSnip/" } })
     end
-  },
-  {
-    "chrisgrieser/nvim-scissors",
-    dependencies = "nvim-telescope/telescope.nvim", -- if using telescope
-    opts = {
-      snippetDir = "~/.config/nvim/lua/LuaSnip/",
-    }
   },
   'saadparwaiz1/cmp_luasnip',
   'rafamadriz/friendly-snippets',

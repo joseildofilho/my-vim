eab48b645dfcbb552ebdb3a330e4051a5f039d31
Joseildo Filho <filhojoseildo@gmail.com>
2025-06-03 14:29:07 -0300

    chore: adds BufEnter to go

--- lua/start.lua
+++ lua/start.lua
@@ -40,6 +40,21 @@ vim.api.nvim_create_autocmd("BufEnter", {
             fidget.notify("We Have some error on trying to load the highlighter by the treesitter", vim.log.levels.ERROR)
             print(err)
         end
+        fidget.notify("Treesitter for HTTP is loaded", vim.log.levels.INFO)
+    end
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = {"*.go"},
+    callback = function(ev)
+        local current_buf = ev.buf
+        vim.treesitter.stop(current_buf)
+        local lang = vim.treesitter.language.get_lang('go')
+        local ok, err = pcall(vim.treesitter.start, current_buf, lang)
+        if not ok then
+            fidget.notify("We Have some error on trying to load the highlighter by the treesitter", vim.log.levels.ERROR)
+            print(err)
+        end
     end
 })
 

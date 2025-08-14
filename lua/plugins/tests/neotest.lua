3d9bd8e86115819db9f1879d8664e83982e16ae6
Joseildo Filho <filhojoseildo@gmail.com>
2025-08-14 16:10:29 -0300

    chore: modifies netotest

--- lua/plugins/tests/neotest.lua
+++ lua/plugins/tests/neotest.lua
@@ -1,58 +1,71 @@
 local jestConfig = {
-    jest_test_discovery = true,
-    jestConfigFile = function()
-        local file = vim.fn.expand('%')
-        if string.find(file, "e2e") then
-            return "jest-integration.config.json"
-        end
-        return vim.fn.getcwd() .. "/jest.config.json"
+  jest_test_discovery = true,
+  jestConfigFile = function()
+    local file = vim.fn.expand('%')
+    if string.find(file, "e2e") then
+      return "jest-integration.config.json"
     end
+    return vim.fn.getcwd() .. "/jest.config.json"
+  end
 }
 
 local goConfig = {
-    dap_go_enabled = true
+  dap_go_enabled = true,
+  sanitize_output = true,
 }
 
 local plenaryConfig = {}
 
 local neotest_lazy_config = {
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        'nvim-neotest/nvim-nio',
-        "nvim-treesitter/nvim-treesitter",
-        "antoinemadec/FixCursorHold.nvim",
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    'nvim-neotest/nvim-nio',
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim",
 
-        "rouge8/neotest-rust",
-        "nvim-neotest/neotest-jest",
-        "fredrikaverpil/neotest-golang",
-        "nvim-neotest/neotest-plenary"
+    "rouge8/neotest-rust",
+    "nvim-neotest/neotest-jest",
+    {
+      "fredrikaverpil/neotest-golang",
+      dependencies = {
+        "uga-rosa/utf8.nvim"
+      }
     },
-    config = function()
-        local neotest = require 'neotest'
-        local jest = require 'neotest-jest'
-        local go = require 'neotest-golang'
-        local plenary = require 'neotest-plenary'
+    "nvim-neotest/neotest-plenary"
+  },
+  config = function()
+    local neotest = require 'neotest'
+    local jest = require 'neotest-jest'
+    local go = require 'neotest-golang'
+    local plenary = require 'neotest-plenary'
 
-        neotest.setup {
-            adapters = {
-                jest(jestConfig),
-                go(goConfig),
-                plenary(plenaryConfig)
-            }
-        }
-
-        require('plugins.tests.tests')
-    end,
-    lazy = true,
-    ft = { "rust", "typescript", "go", "lua" },
-    opts = {
-        discovery = {
-            enabled = false
-        }
+    neotest.setup {
+      adapters = {
+        jest(jestConfig),
+        go(goConfig),
+        plenary(plenaryConfig)
+      }
     }
+
+    require('plugins.tests.tests')
+  end,
+  lazy = true,
+  ft = { "rust", "typescript", "go", "lua" },
+  opts = {
+    discovery = {
+      enabled = true,
+      concurrent = 1,
+    },
+    output_panel = {
+        enabled = false,
+    },
+    summary = {
+      open = "botright vsplit | vertical resize 50 | set relativenumber number | set nowrap"
+    }
+  }
 }
 
 return {
-    neotest_lazy_config
+  neotest_lazy_config
 }

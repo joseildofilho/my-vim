e3a05a537d3e37da7af4e9a6493360b8f0ab92ee
Joseildo Filho <filhojoseildo@gmail.com>
2025-10-15 15:40:17 -0300

    chore: removes duplicated CopilotChat

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,18 +28,6 @@ local plugins = {
     },
   },
   {
-    {
-      "CopilotC-Nvim/CopilotChat.nvim",
-      dependencies = {
-        { "nvim-lua/plenary.nvim", branch = "master" },
-      },
-      build = "make tiktoken",
-      opts = {
-        -- See Configuration section for options
-      },
-    },
-  },
-  {
     "kndndrj/nvim-dbee",
     dependencies = {
       "MunifTanjim/nui.nvim",

06903a9a01ab7942c87b4a6d724fa410c30691e3
Joseildo Filho <filhojoseildo@gmail.com>
2025-10-30 10:27:22 -0300

    fix: fixes ignored files for telescopes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 require('telescope').setup{
     defaults = {
-        file_ignore_patterns = { "node%_modules/.*", "dist/.*", "build/.*", "deps/.*" },
+        file_ignore_patterns = { "node_modules/.*", "dist/.*", "build/.*", "deps/.*" },
         path_display = {"smart"}
     },
 }

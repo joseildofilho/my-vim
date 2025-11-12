21f943888d45ea67db0e9766ca54af831f761174
Joseildo Filho <filhojoseildo@gmail.com>
2025-11-11 23:00:56 -0300

    chore: remove unused or redundant lsps

--- lsp/ts_ls.lua
+++
@@ -1,5 +0,0 @@
-return {
-  filetypes = { 'typescript', 'javascript' },
-  root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
-  cmd = { 'typescript-language-server', '--stdio' }
-}

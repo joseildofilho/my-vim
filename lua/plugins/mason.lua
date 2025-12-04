3dac26592d55bc80cc468f3ec7ad7d6355555673
joseildo filho <filhojoseildo@gmail.com>
2025-12-03 23:08:37 -0300

    chore: adds gopls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,7 +6,7 @@ return {
   {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     opts = {
-      ensure_installed = { 'lua-language-server' },
+      ensure_installed = { 'lua-language-server', 'gopls' },
     }
   }
 }

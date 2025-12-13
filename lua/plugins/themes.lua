547fbd1c145f3562fcacb43abee311c501fac7e4
Joseildo Filho <filhojoseildo@gmail.com>
2025-12-13 13:44:36 -0300

    chore: adds neg theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,14 +1,19 @@
 return {
-    'rebelot/kanagawa.nvim',
-    {
-        'folke/tokyonight.nvim'
-    },
-    {
-        'vague2k/vague.nvim',
-        opts = {
-          style = {
-            strings = "none",
-          }
-        }
+  'rebelot/kanagawa.nvim',
+  {
+    'folke/tokyonight.nvim'
+  },
+  {
+    'vague2k/vague.nvim',
+    opts = {
+      style = {
+        strings = "none",
+      }
     }
+  },
+  {
+    'neg-serg/neg.nvim',
+    lazy = false,
+    priority = 1000,
+  }
 }

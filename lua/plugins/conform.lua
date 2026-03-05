f2357a43d1fe526100bb3e38f4b9faaf19b0057d
Joseildo Filho <filhojoseildo@gmail.com>
2026-03-05 09:58:35 -0300

    chore: adds lua to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,7 @@ return {
   'stevearc/conform.nvim',
   opts = {
     formatters_by_ft = {
+      lua = { lsp_format = "fallback" },
       java = { 'google-java-format' },
       go = { 'gofmt' },
       dart = { 'fvm dart format write', lsp_format = "fallback" }

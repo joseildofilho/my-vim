ad4d47a46017604c3a6b0ff531dbb08c2ecc93ac
Joseildo Filho <filhojoseildo@gmail.com>
2026-03-05 09:39:19 -0300

    chore: improves the jdtls

--- lua/plugins/jdtls/jdtls_setup.lua
+++ lua/plugins/jdtls/jdtls_setup.lua
@@ -44,7 +44,16 @@ function M:setup()
     -- Here you can configure eclipse.jdt.ls specific settings
     -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
     -- for a list of options
-    settings = {},
+    settings = {
+      java = {
+        compiler = {
+          annotationProcessing = {
+            enabled = true,
+            executeInEditor = true,
+          }
+        }
+      }
+    },
 
     -- Language server `initializationOptions`
     -- You need to extend the `bundles` with paths to jar files

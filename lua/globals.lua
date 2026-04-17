5f9ee5d25e31c5d4f32464977f28f40f24b25530
Joseildo Filho <filhojoseildo@gmail.com>
2026-04-17 14:07:20 -0300

    chore: creates the new gh function

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,8 +1,10 @@
 function P(opts)
-    print(vim.inspect(opts))
+  print(vim.inspect(opts))
 end
 
 function FORCE_RELOAD(name)
-    package.loaded[name] = nil
-    return require(name)
+  package.loaded[name] = nil
+  return require(name)
 end
+
+GH = function(x) return 'https://github.com/' .. x end

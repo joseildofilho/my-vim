e3b88822ea346b03b5dacfbe0ded836d5285ff46
Joseildo Filho <filhojoseildo@gmail.com>
2026-05-04 10:20:51 -0300

    chore: suports warn-burnout

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -16,5 +16,12 @@ return {
     lazy = false,
     priority = 1000,
   },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'felipefdl/warm-burnout',
+    priority = 1000,
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. '/nvim')
+    end,
+  }
 }
